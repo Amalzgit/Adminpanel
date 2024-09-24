2ae27ae928a438ef7e9b14a9b8472131c7c8d85f
Amalzgit <1mailtoamal@gmail.com>
2024-09-24 12:27:41 +0530

    admin pannel

--- middilewares/methodOverrideMiddleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = (getterQuery)=>{
-  return (req,res,next)=>{
-    if(req.query[getterQuery] && ['GET','POST','PUT','PATCH','DELETE'].includes(req.query[getterQuery].toUpperCase())){
-      req.method = req.query[getterQuery];
-      delete req.query[getterQuery];
-    }
-    next();
-  }
-}
--- middilewares/noCacheMiddleware.js
+++
@@ -1,4 +0,0 @@
-module.exports = (req,res,next)=>{
-  res.setHeader('Cache-Control','no-cache, no-store,must-revalidate');
-  next()
-}
